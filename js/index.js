6342688c22e8f768e7bd91cb633ea18f8e742347
dansch1982 <dansch1982@gmail.com>
2022-03-20 14:39:07 +0100

    sum

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('service-worker.js');
+        navigator.serviceWorker.register('./service-worker.js');
     }
 }
 
