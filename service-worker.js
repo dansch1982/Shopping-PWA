a98ae002e02032f77a19c5d7dd0b6490c6bb1933
dansch1982 <dansch1982@gmail.com>
2022-03-20 15:02:06 +0100

    sum

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-importScripts(
+/* importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-);
+); */
 
 const cacheName = 'Shopping';
 const filesToCache = [
@@ -20,13 +20,13 @@ self.addEventListener('install', event => {
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
 /* self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+}); */
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
-}); */
+});
